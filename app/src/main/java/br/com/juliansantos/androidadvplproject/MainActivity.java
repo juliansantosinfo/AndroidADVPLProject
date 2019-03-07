8552d7b29108c5f71335b63fb7286d969a89c472
Julian A. Santos <julian.santos.info@gmail.com>
2019-03-07 01:03:01 -0300

    ...

--- app/src/main/java/br/com/juliansantos/androidadvplproject/MainActivity.java
+++ app/src/main/java/br/com/juliansantos/androidadvplproject/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.juliansantos.androidadvplproject;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
-import br.com.juliansantos.androidadvplproject.R;
 import br.com.juliansantos.androidadvplproject.beans.CompanyProtheus;
 import br.com.juliansantos.androidadvplproject.beans.UserProtheus;
 import br.com.juliansantos.androidadvplproject.webservices.WSAuthenticationProtheus;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Global variables.
     private LinearLayout layoutMain;
+    private ProgressBar progressBarLogin;
     private LinearLayout layoutTop;
     private TextView txvTitle;
     private LinearLayout layoutCenter;
@@ -66,9 +68,6 @@ public class MainActivity extends AppCompatActivity {
         // load views to class.
         initViews();
 
-        // Initialize listener for buttons.
-        initButtonListener();
-
     }
 
     /**
@@ -96,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
 
         layoutMain = findViewById(R.id.layout_main);
 
+        progressBarLogin = findViewById(R.id.pgrb_login);
+
         layoutTop = findViewById(R.id.layout2_top);
         txvTitle = findViewById(R.id.txv_login);
 
@@ -114,58 +115,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Method for initialize listener in buttons.
+     * Method for initialize listener in button entrar.
      *
      * @since 04/03/2019
      */
-    private void initButtonListener() {
+    public void btnEntrarOnClick(View view) {
 
-        btnSingIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // Local variables.
+        String userCode = edtUser.getText().toString();
+        String pass = edtPass.getText().toString();
 
-                // Local variables.
-                String userCode = "mas800109";
-                String pass = "Cecor@2009";
+        WSAuthenticationProtheus wsAuthenticationProtheus;
+        WSUserProtheus wsUserProtheus;
 
-                // Initializes user and password
-                userCode = edtUser.getText().toString();
-                pass = edtPass.getText().toString();
-                pass = Base64.encodeToString(pass.getBytes(), Base64.DEFAULT);
+        // Initializes user and password
+        userCode = edtUser.getText().toString();
+        pass = edtPass.getText().toString();
+        pass = Base64.encodeToString(pass.getBytes(), Base64.DEFAULT);
 
-                try {
+        try {
 
-                    // Performs authentication for user and password.
-                    isAuthorized = (boolean) new WSAuthenticationProtheus(userCode, pass).execute().get();
+            Toast toast0 = Toast.makeText(MainActivity.this, "INICIO", Toast.LENGTH_SHORT);
+            toast0.show();
 
-                    // Is successfully logged, fetches user information.
-                    if (isAuthorized) {
-                        user = (UserProtheus) new WSUserProtheus("cod", userCode).execute().get();
+            // Performs authentication for user and password.
+            wsAuthenticationProtheus = new WSAuthenticationProtheus(userCode, pass);
+            wsAuthenticationProtheus.execute();
 
-                        // If not load user information.
-                        if (user.getId().isEmpty()) {
-                            return;
-                        }
+            Toast toast1 = Toast.makeText(this, "MEIO", Toast.LENGTH_SHORT);
+            toast1.show();
 
-                        // Get available company`s for protheus user.
-                        companysProtheus = new ArrayList<CompanyProtheus>(Arrays.asList((CompanyProtheus[]) new WSUserProtheus("emp", userCode).execute().get()));
+            isAuthorized = wsAuthenticationProtheus.get();
 
-                        //Next
-                        Toast toast = Toast.makeText(getApplicationContext(), "Olâ " + user.getName(), Toast.LENGTH_LONG);
-                        toast.show();
+            Toast toast2 = Toast.makeText(getApplicationContext(), "FIM", Toast.LENGTH_SHORT);
+            toast2.show();
 
-                    } else {
-                        return;
-                    }
 
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            // Is successfully logged, fetches user information.
+            if (isAuthorized) {
+                user = (UserProtheus) new WSUserProtheus("cod", userCode).execute().get();
+
+                // If not load user information.
+                if (user.getId().isEmpty()) {
+                    return;
                 }
 
+                // Get available company`s for protheus user.
+                companysProtheus = new ArrayList<CompanyProtheus>(Arrays.asList((CompanyProtheus[]) new WSUserProtheus("emp", userCode).execute().get()));
+
+                //Next
+                Toast toast = Toast.makeText(getApplicationContext(), "Olâ " + user.getName(), Toast.LENGTH_LONG);
+                toast.show();
+
+            } else {
+                return;
             }
-        });
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/br/com/juliansantos/androidadvplproject/webservices/WSAuthenticationProtheus.java
+++ app/src/main/java/br/com/juliansantos/androidadvplproject/webservices/WSAuthenticationProtheus.java
@@ -1,6 +1,11 @@
 package br.com.juliansantos.androidadvplproject.webservices;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -9,7 +14,9 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Scanner;
 
-public class WSAuthenticationProtheus extends AsyncTask {
+import br.com.juliansantos.androidadvplproject.MainActivity;
+
+public class WSAuthenticationProtheus extends AsyncTask<Void, Void, Boolean> {
 
     private String urlPath = "http://{ipServer}:{portServer}/REST/GET/JWSRAUTH/auth/{user}/{pass}";
     private String ipServer = "187.94.62.92";
@@ -27,8 +34,32 @@ public class WSAuthenticationProtheus extends AsyncTask {
         this.pass = pass;
     }
 
+    /**
+     * Runs on the UI thread before {@link #doInBackground}.
+     *
+     * @see #onPostExecute
+     * @see #doInBackground
+     */
     @Override
-    protected Object doInBackground(Object[] objects) {
+    protected void onPreExecute() {
+    }
+
+    /**
+     * Override this method to perform a computation on a background thread. The
+     * specified parameters are the parameters passed to {@link #execute}
+     * by the caller of this task.
+     *
+     * This method can call {@link #publishProgress} to publish updates
+     * on the UI thread.
+     *
+     * @return A result, defined by the subclass of this task.
+     *
+     * @see #onPreExecute()
+     * @see #onPostExecute
+     * @see #publishProgress
+     */
+    @Override
+    protected Boolean doInBackground(Void... voids) {
 
         // Define url for request.
         urlPath = urlPath.replace("{ipServer}", ipServer);
@@ -69,4 +100,16 @@ public class WSAuthenticationProtheus extends AsyncTask {
         return Boolean.parseBoolean(httpReturn.toString());
     }
 
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * @param isAuthorized The result of the operation computed by {@link #doInBackground}.
+     */
+    @Override
+    protected void onPostExecute(Boolean isAuthorized) {
+
+    }
 }
